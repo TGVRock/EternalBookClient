3044521f5e9e26179f597a89c088d29d4640b44b
TGVRock <85382178+TGVRock@users.noreply.github.com>
2023-03-08 22:18:28 +0900

    i18n 移動

--- src/views/Writer/CreateMosaicProcessingView.vue
+++ src/views/Writer/CreateMosaicProcessingView.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import { ref, watch } from "vue";
+import { useI18n } from "vue-i18n";
 import ProcessingComponent from "@/components/Progress/ProcessingComponent.vue";
 import WriteCompleteComponent from "@/components/Progress/CreateMosaicCompleteComponent.vue";
 import { useEnvironmentStore } from "@/stores/environment";
 import { useWriteMosaicStore } from "@/stores/WriteMosaic";
-import { getWriteProgreassMessage } from "@/utils/converter";
 import { WriteProgress } from "@/models/enums/WriteProgress";
+
+// Locale
+const i18n = useI18n();
 
 // Stores
 const envStore = useEnvironmentStore();
@@ -30,6 +33,43 @@ watch(
 
 // モザイク作成
 writeMosaicStore.createMosaic();
+
+/**
+ * 書き込み状況に対応したメッセージを取得する
+ * @param progress 書き込み状況
+ * @returns 対応メッセージ
+ */
+function getWriteProgreassMessage(progress: WriteProgress): string {
+  switch (progress) {
+    case WriteProgress.Preprocess:
+      return i18n.t("message.processing");
+    case WriteProgress.LockSigning:
+      return i18n.t("message.lockSigning");
+    case WriteProgress.LockAnnounced:
+      return i18n.t("message.lockAnnounced");
+    case WriteProgress.LockUnconfirmed:
+      return i18n.t("message.lockUnconfirmed");
+    case WriteProgress.LockConfirmed:
+      return i18n.t("message.lockConfirmed");
+    case WriteProgress.TxSigning:
+      return i18n.t("message.txSigning");
+    case WriteProgress.TxAnnounced:
+      return i18n.t("message.txAnnounced");
+    case WriteProgress.TxWaitCosign:
+      return i18n.t("message.txWaitCosign");
+    case WriteProgress.TxUnconfirmed:
+      return i18n.t("message.txUnconfirmed");
+    case WriteProgress.TxConfirmed:
+      return i18n.t("message.txConfirmed");
+    case WriteProgress.Complete:
+      return i18n.t("message.complete");
+    case WriteProgress.Failed:
+      return i18n.t("message.failed");
+    case WriteProgress.Standby:
+    default:
+      return i18n.t("message.prepare");
+  }
+}
 </script>
 
 <template>
--- src/views/Writer/OnChainDataProcessingView.vue
+++ src/views/Writer/OnChainDataProcessingView.vue
@@ -1,12 +1,15 @@
 <script setup lang="ts">
 import { ref, watch } from "vue";
+import { useI18n } from "vue-i18n";
 import ProcessingComponent from "@/components/Progress/ProcessingComponent.vue";
 import WriteOnChainCompleteComponent from "@/components/Progress/WriteOnChainCompleteComponent.vue";
 import { useEnvironmentStore } from "@/stores/environment";
 import { useWriteOnChainDataStore } from "@/stores/WriteOnChainData";
 import CONSTS from "@/utils/consts";
-import { getWriteProgreassMessage } from "@/utils/converter";
 import { WriteProgress } from "@/models/enums/WriteProgress";
+
+// Locale
+const i18n = useI18n();
 
 // Stores
 const envStore = useEnvironmentStore();
@@ -21,25 +24,7 @@ watch(
   () => {
     const logTitle = "write data progress watch:";
     envStore.logger.debug(logTitle, "start", writeOnChainDataStore.progress);
-    const entire = Math.ceil(
-      writeOnChainDataStore.dataBase64.length /
-        (CONSTS.TX_DATASIZE_PER_TRANSFER * CONSTS.TX_DATA_TX_NUM)
-    );
-    const proceed = Math.ceil(
-      writeOnChainDataStore.processedSize /
-        writeOnChainDataStore.dataBase64.length
-    );
-    const dataProgress = proceed.toString() + " / " + entire.toString() + " ";
-    if (
-      writeOnChainDataStore.progress === WriteProgress.Standby ||
-      writeOnChainDataStore.progress === WriteProgress.Failed ||
-      writeOnChainDataStore.progress === WriteProgress.Complete
-    ) {
-      message.value = getWriteProgreassMessage(writeOnChainDataStore.progress);
-    } else {
-      message.value =
-        dataProgress + getWriteProgreassMessage(writeOnChainDataStore.progress);
-    }
+    message.value = getWriteProgreassMessage(writeOnChainDataStore.progress);
     envStore.logger.debug(logTitle, "end", message.value);
   },
   {
@@ -49,6 +34,52 @@ watch(
 
 // オンチェーンデータ書き込み
 writeOnChainDataStore.writeOnChain();
+
+/**
+ * 書き込み状況に対応したメッセージを取得する
+ * @param progress 書き込み状況
+ * @returns 対応メッセージ
+ */
+function getWriteProgreassMessage(progress: WriteProgress): string {
+  const entire = Math.ceil(
+    writeOnChainDataStore.dataBase64.length /
+      (CONSTS.TX_DATASIZE_PER_TRANSFER * CONSTS.TX_DATA_TX_NUM)
+  );
+  const proceed = Math.ceil(
+    writeOnChainDataStore.processedSize /
+      writeOnChainDataStore.dataBase64.length
+  );
+  const dataProgress = proceed.toString() + " / " + entire.toString() + " ";
+  switch (progress) {
+    case WriteProgress.Preprocess:
+      return dataProgress + i18n.t("message.processing");
+    case WriteProgress.LockSigning:
+      return dataProgress + i18n.t("message.lockSigning");
+    case WriteProgress.LockAnnounced:
+      return dataProgress + i18n.t("message.lockAnnounced");
+    case WriteProgress.LockUnconfirmed:
+      return dataProgress + i18n.t("message.lockUnconfirmed");
+    case WriteProgress.LockConfirmed:
+      return dataProgress + i18n.t("message.lockConfirmed");
+    case WriteProgress.TxSigning:
+      return dataProgress + i18n.t("message.txSigning");
+    case WriteProgress.TxAnnounced:
+      return dataProgress + i18n.t("message.txAnnounced");
+    case WriteProgress.TxWaitCosign:
+      return dataProgress + i18n.t("message.txWaitCosign");
+    case WriteProgress.TxUnconfirmed:
+      return dataProgress + i18n.t("message.txUnconfirmed");
+    case WriteProgress.TxConfirmed:
+      return dataProgress + i18n.t("message.txConfirmed");
+    case WriteProgress.Complete:
+      return i18n.t("message.complete");
+    case WriteProgress.Failed:
+      return i18n.t("message.failed");
+    case WriteProgress.Standby:
+    default:
+      return i18n.t("message.prepare");
+  }
+}
 </script>
 
 <template>
