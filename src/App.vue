373040ea4ce5b531ad1fdd0fa4fb562e76f3bf31
TGVRock <85382178+TGVRock@users.noreply.github.com>
2023-03-17 00:23:31 +0900

    ヘッダーをコンポネント化

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,9 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from "vue-router";
-import { useSSSStore } from "./stores/sss";
-import LocaleMenuComponent from "@/components/header/LocaleMenuComponent.vue";
-import NetworkTypeMenuComponent from "@/components/header/NetworkTypeMenuComponent.vue";
-
-// Stores
-const sssStore = useSSSStore();
+import { RouterView } from "vue-router";
+import HeaderComponent from "./components/header/HeaderComponent.vue";
 </script>
 
 <template>
-  <header class="sticky-top">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-      <div class="container-fluid">
-        <RouterLink class="navbar-brand" v-bind:to="{ name: 'home' }">
-          EternalBookClient
-        </RouterLink>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-            <li class="nav-item">
-              <RouterLink class="nav-link" v-bind:to="{ name: 'WriterTop' }">
-                Writer
-              </RouterLink>
-            </li>
-            <li class="nav-item">
-              <RouterLink class="nav-link" v-bind:to="{ name: 'ViewerTop' }">
-                Viewer
-              </RouterLink>
-            </li>
-          </ul>
-          <div class="d-flex me-2">
-            <NetworkTypeMenuComponent />
-          </div>
-          <div v-if="sssStore.sssLinked" class="d-flex me-2">
-            {{ sssStore.address }}
-          </div>
-          <div class="d-flex me-2">
-            <LocaleMenuComponent />
-          </div>
-        </div>
-      </div>
-    </nav>
-  </header>
-
+  <HeaderComponent />
   <RouterView />
 </template>
-
-<!-- <style scoped>
-</style> -->
---
+++ src/components/header/HeaderComponent.vue
@@ -0,0 +1,75 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import { RouterLink } from "vue-router";
+import LocaleMenuComponent from "@/components/header/LocaleMenuComponent.vue";
+import NetworkTypeMenuComponent from "@/components/header/NetworkTypeMenuComponent.vue";
+
+// Reactives
+const isShow = ref(false);
+
+/**
+ * 閉じるクリックイベント
+ */
+function onClickClose(): void {
+  isShow.value = false;
+}
+
+/**
+ * トグルクリックイベント
+ */
+function onClickToggle(): void {
+  isShow.value = !isShow.value;
+}
+</script>
+
+<template>
+  <nav class="navbar sticky-top navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <RouterLink
+        class="navbar-brand"
+        v-bind:to="{ name: 'home' }"
+        v-on:click="onClickClose"
+      >
+        EternalBookClient
+      </RouterLink>
+      <button
+        class="navbar-toggler"
+        type="button"
+        v-on:click.stop="onClickToggle"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" v-bind:class="{ show: isShow }">
+        <div class="navbar-nav me-auto my-2 my-lg-0 ms-2 ms-lg-0">
+          <RouterLink
+            class="nav-link ps-2"
+            v-bind:to="{ name: 'WriterTop' }"
+            v-on:click="onClickClose"
+          >
+            Writer
+          </RouterLink>
+          <RouterLink
+            class="nav-link ps-2"
+            v-bind:to="{ name: 'ViewerTop' }"
+            v-on:click="onClickClose"
+          >
+            Viewer
+          </RouterLink>
+        </div>
+        <div class="d-flex">
+          <NetworkTypeMenuComponent class="me-2" />
+          <LocaleMenuComponent class="me-2" />
+        </div>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+.router-link-active {
+  background-color: rgba(200, 219, 241, 0.678);
+  color: rgba(210, 112, 255, 0.3);
+  cursor: default;
+  pointer-events: none;
+}
+</style>
