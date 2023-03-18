0489c978a1442ae0322c06072cf7eb68020fe5ef
TGVRock <85382178+TGVRock@users.noreply.github.com>
2023-03-18 20:01:17 +0900

    モザイク新規作成領域と既存モザイク紐付け領域をコンポーネント化

---
+++ src/components/OnChainData/RelatedMosaicAreaComponent.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import { useWriteOnChainDataStore } from "@/stores/WriteOnChainData";
+import TextAreaComponent from "@/components/form/TextAreaComponent.vue";
+import TransitionButtonComponent from "@/components/form/TransitionButtonComponent.vue";
+
+// Stores
+const writeOnChainDataStore = useWriteOnChainDataStore();
+</script>
+
+<template>
+  <section>
+    <TextAreaComponent
+      v-bind:item-name="$t('mosaicInfo.id')"
+      v-bind:placeholder="
+        $t('writer.pleaseInputItem', { item: $t('mosaicInfo.id') })
+      "
+      v-model:value="writeOnChainDataStore.relatedMosaicIdStr"
+    />
+    <TransitionButtonComponent
+      class="text-center"
+      v-bind:next-route-name="`WriteOnChainData`"
+      v-bind:item-name="$t(`writer.writeOnChain`)"
+    />
+  </section>
+</template>
--- src/views/Writer/TopView.vue
+++ src/views/Writer/TopView.vue
@@ -1,18 +1,15 @@
 <script setup lang="ts">
 import { useSSSStore } from "@/stores/sss";
 import { useWriteMosaicStore } from "@/stores/WriteMosaic";
-import { useWriteOnChainDataStore } from "@/stores/WriteOnChainData";
 import InputAreaComponent from "@/components/OnChainData/InputAreaComponent.vue";
+import RelatedMosaicAreaComponent from "@/components/OnChainData/RelatedMosaicAreaComponent.vue";
+import CreateMosaicAreaComponent from "@/components/MosaicInfo/CreateMosaicAreaComponent.vue";
 import TextAreaComponent from "@/components/form/TextAreaComponent.vue";
-import NumberAreaComponent from "@/components/form/NumberAreaComponent.vue";
-import MosaicFlagAreaComponent from "@/components/form/MosaicFlagAreaComponent.vue";
 import SSSLinkedSelectAreaComponent from "@/components/form/SSSLinkedSelectAreaComponent.vue";
-import TransitionButtonComponent from "@/components/form/TransitionButtonComponent.vue";
 
 // Stores
 const sssStore = useSSSStore();
 const writeMosaicStore = useWriteMosaicStore();
-const writeOnChainDataStore = useWriteOnChainDataStore();
 
 writeMosaicStore.ownerAddress = "";
 </script>
@@ -31,35 +28,7 @@ writeMosaicStore.ownerAddress = "";
       />
     </section>
     <InputAreaComponent />
-    <section>
-      <MosaicFlagAreaComponent />
-      <NumberAreaComponent
-        v-bind:item-name="$t('mosaicInfo.supply')"
-        v-bind:placeholder="
-          $t('writer.pleaseInputItem', { item: $t('mosaicInfo.supply') })
-        "
-        v-model:value="writeMosaicStore.amount"
-        v-bind:min="1"
-      />
-      <TransitionButtonComponent
-        class="text-center"
-        v-bind:next-route-name="`CreateMosaic`"
-        v-bind:item-name="$t(`writer.createMosaic`)"
-      />
-    </section>
-    <section>
-      <TextAreaComponent
-        v-bind:item-name="$t('mosaicInfo.id')"
-        v-bind:placeholder="
-          $t('writer.pleaseInputItem', { item: $t('mosaicInfo.id') })
-        "
-        v-model:value="writeOnChainDataStore.relatedMosaicIdStr"
-      />
-      <TransitionButtonComponent
-        class="text-center"
-        v-bind:next-route-name="`WriteOnChainData`"
-        v-bind:item-name="$t(`writer.writeOnChain`)"
-      />
-    </section>
+    <CreateMosaicAreaComponent />
+    <RelatedMosaicAreaComponent />
   </article>
 </template>
