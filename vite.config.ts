735a1c787dcf2b9cfa3fe80447e874292f37e300
TGVRock <85382178+TGVRock@users.noreply.github.com>
2023-03-27 03:23:16 +0900

    GitHub Pages 公開用に base 設定

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,7 @@ export default defineConfig({
       protocolImports: true,
     }),
   ],
+  base: "/EternalBookClient",
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
